955d64e584b66caed9d577d13b5f0e0269aaa95e
unknown <sandeepkgupta007@gmail.com>
2018-04-20 15:56:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.text({type: '*/*'}));
+const request = require('request');
+
+let npci_root = "https://103.114.161.148";
+let switch_root = "http://localhost:3000";
+
+// app.post('/upi/ReqPay', (req, res) =>{
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    request.post(
+        {
+            url:npci_root,
+            body : "hello",
+            method:"POST",
+            rejectUnauthorized: false,
+            headers:{
+                'Content-Type': 'application/xml',
+            }, //todo: any encryption to add here, I think not.
+        },
+        (err, res, body) => {
+            console.log('got response from upi',body);
+            console.log('got error from upi',err);
+        });
+
+    // res.send(req.body);
+// });
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
