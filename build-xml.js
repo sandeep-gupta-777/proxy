7c798f03bc3f88137c61e84a68eb27e5affe06cc
sandeep kumar gupta <sandeepkgupta007@gmail.com>
2018-05-15 03:37:12 +0530

    test

--- build-xml.js
+++ build-xml.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const uuidv1 = require('uuid/v1');
 
 (function(){
+    var d = new Date();
+    var ts = d.toISOString();    
     let uuidStr = "ICI"+ uuidv1().replace(/-/g, "");
-    let xmlStr = `<upi:ReqHbt xmlns:upi="http://npci.org/upi/schema/"><Head ver="1.0" ts="2018-03-07T17:58:54+05:30" orgId="400011" msgId="${uuidStr}"/><Txn id="${uuidStr}" note="taxi-bill" refId="${uuidStr}" custRef="806717014630" refUrl="https://mystar.com/orderid" ts="2018-03-07T17:58:54+05:30" type="PAY"></Txn><HbtMsg type="EODE" value="DATE"/></upi:ReqHbt>`;
+    let xmlStr = `<upi:ReqHbt xmlns:upi="http://npci.org/upi/schema/"><Head ver="1.0" ts="${ts}" orgId="400011" msgId="${uuidStr}"/><Txn id="${uuidStr}" note="taxi-bill" refId="${uuidStr}" custRef="806711014630" refUrl="https://mystar.com/orderid" ts="${ts}" type="PAY"></Txn><HbtMsg type="EODE" value="${ts}"/></upi:ReqHbt>`;
     
     console.log(xmlStr);
     fs.writeFileSync(`C:\\Users\\384041\\Desktop\\nodebook\\proxy\\sign-java\\sign\\src\\ReqHbt.xml`, xmlStr);
